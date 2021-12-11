534220788f15eec383bf9357bb513d440163e879
Klemen Pravdič <klemen.pravdic@arnes.si>
2021-12-11 01:52:27 +0100

    backend apis

---
+++ src/main/java/com/klepra/ngbootupload/NgbootuploadApplication.java
@@ -0,0 +1,13 @@
+package com.klepra.ngbootupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgbootuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgbootuploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klepra/ngbootupload/NgbootuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klepra.ngbootupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgbootuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
